fce86d8a74ba9ebf0688664332f6eea1e6f8b7ca
unknown <chenrunjie2002@gmail.com>
2024-02-01 23:20:48 -0500

    removed extranous links in mobile version of menu

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -71,7 +71,7 @@ export default function MobileMenu() {
         <ul className="bg-gray-800 px-4 py-2">
           <li>
             <Link
-              href="/signin"
+              href="/#"
               className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center"
               onClick={() => setMobileNavOpen(false)}
             >
@@ -80,7 +80,7 @@ export default function MobileMenu() {
           </li>
           <li>
             <Link
-              href="/signin"
+              href="/#"
               className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center"
               onClick={() => setMobileNavOpen(false)}
             >
@@ -89,7 +89,7 @@ export default function MobileMenu() {
           </li>
           <li>
             <Link
-              href="/signup"
+              href="/#"
               className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out"
               onClick={() => setMobileNavOpen(false)}
             >
