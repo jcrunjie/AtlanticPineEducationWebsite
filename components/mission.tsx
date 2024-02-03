e0f13bd7337bda07c3a348eec20bca95e413af69
unknown <chenrunjie2002@gmail.com>
2024-02-02 22:35:37 -0500

    fixed about page compatibility on mobile

--- components/mission.tsx
+++ components/mission.tsx
@@ -1,26 +1,22 @@
 import Image from "next/image";
-
-import FeatImage01 from "@/public/images/features-03-image-01.png";
-import FeatImage02 from "@/public/images/features-03-image-02.png";
 import LandingImage from "@/public/images/landingPagePlaceHolder.svg";
-import FeatImage03 from "@/public/images/features-03-image-03.png";
 
 export default function Mission() {
   return (
     <section>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 my-6">
         <div className="py-12 md:py-20 border-gray-800">
           {/* Items */}
           <div className="grid gap-20"></div>
           <div className="container">
             <div className="column">
               <div
-                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 bg-purple-600 rounded-corners"
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 bg-purple-600 rounded-corners mb-4"
                 data-aos="fade-right"
               >
-                <div className="md:pr-4 lg:pr-12 xl:pr-16 xl:pl-16 pt-5">
-                  <h3 className="h3">Mission</h3>
-                  <p className="text-xl text-purple-200 mb-4 py-3">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16 xl:pl-16 pt-5 md:order-1">
+                  <h3 className="h3 px-8">Mission</h3>
+                  <p className="text-xl text-purple-200 mb-4 py-3 px-8">
                     Atlantic Pine Education Company is committed to becoming a
                     bridge and promoter of Sino-American cultural exchange. Our
                     mission is to help Chinese students achieve comprehensive
@@ -36,23 +32,21 @@ export default function Mission() {
                 </div>
               </div>
               <div
-                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-4 md:mb-0 rtl"
                 data-aos="fade-up"
               >
                 <Image
-                  className="max-w-full mx-auto md:max-w-none h-auto rounded-corners"
+                  className="max-w-full mx-auto md:max-w-none h-auto rounded-corners mb-4"
                   src={LandingImage}
                   width={540}
                   height={405}
                   alt="Features 02"
                 />
               </div>
-              {/* <Image className="image" src="https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg" alt="Image 1"> */}
             </div>
-
             <div className="column">
               <div
-                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-4 md:mb-0 rtl"
                 data-aos="fade-up"
               >
                 <Image
@@ -67,9 +61,9 @@ export default function Mission() {
                 className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 bg-purple-700 rounded-corners"
                 data-aos="fade-right"
               >
-                <div className="md:pr-4 lg:pr-12 xl:pr-16 xl:pl-16 pt-5">
-                  <h3 className="h3 text-white">Vision</h3>
-                  <p className="text-xl text-purple-200 mb-4 py-3">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16 xl:pl-16 pt-5 md:order-1">
+                  <h3 className="h3 text-white px-8">Vision</h3>
+                  <p className="text-xl text-purple-200 mb-4 py-3 px-8">
                     Our vision is to break down cultural barriers and build a
                     more harmonious, respectful, and understanding relationship
                     between China and the United States through educational
